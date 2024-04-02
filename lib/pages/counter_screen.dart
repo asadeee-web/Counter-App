14c67442bea74c05d7dadf5dc05210a88ad5c131
Asad Qureshi <asadeee998@gmail.com>
2024-04-02 10:11:13 +0500

    updated

--- lib/pages/counter_screen.dart
+++ lib/pages/counter_screen.dart
@@ -8,14 +8,55 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
+  int count = 0;
+  void _increment() {
+    setState(() {
+      count++;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      count--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-        title: Text("Counter App"),
-      )),
+        appBar: AppBar(
+          backgroundColor: Colors.purple[100],
+          title: Center(child: Text("Counter App")),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("You Pushed The Button This Many Time: "),
+            Text(
+              count.toString(),
+              style: TextStyle(fontSize: 40),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      _increment();
+                    },
+                    child: Text("Increment")),
+                ElevatedButton(
+                    onPressed: () {
+                      _decrement();
+                    },
+                    child: Text("Decrement"))
+              ],
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
