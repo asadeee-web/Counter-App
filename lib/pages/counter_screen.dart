f4254f8bddbbb226b25a1cd889aaf5b829a4f391
Asad Qureshi <asadeee998@gmail.com>
2024-04-02 09:57:08 +0500

    initial

---
+++ lib/pages/counter_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+        title: Text("Counter App"),
+      )),
+    );
+  }
+}
